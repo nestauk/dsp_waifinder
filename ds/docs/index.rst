b20492f9a3574e3c2c2f633e774002abdd14c1df
lizgzil <lizgzil@hotmail.com>
2022-02-18 15:50:01 +0000

    Add cookiecutter ds folder

---
+++ ds/docs/index.rst
@@ -0,0 +1,19 @@
+.. toctree::
+   :hidden:
+   :maxdepth: 1
+
+   api/ds
+   License <license>
+
+ds
+===============================
+
+Data Science for the AI map project
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ ds/setup.py
@@ -0,0 +1,26 @@
+"""ds."""
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read_lines(path):
+    """Read lines of `path`."""
+    with open(path) as f:
+        return f.read().splitlines()
+
+
+BASE_DIR = Path(__file__).parent
+
+
+setup(
+    name="ds",
+    long_description=open(BASE_DIR / "README.md").read(),
+    install_requires=read_lines(BASE_DIR / "requirements.txt"),
+    extras_require={"dev": read_lines(BASE_DIR / "requirements_dev.txt")},
+    packages=find_packages(exclude=["docs"]),
+    version="0.1.0",
+    description="Data Science for the AI map project",
+    author="Nesta",
+    license="proprietary",
+)
