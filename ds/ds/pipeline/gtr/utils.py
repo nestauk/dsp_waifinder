10f532289160cefb5af91a77f3d87039bcb7814b
lizgzil <lizgzil@hotmail.com>
2022-02-23 16:49:37 +0000

    Additions to gtr utils including sql queries and connecting to db

--- ds/ds/pipeline/gtr/utils.py
+++ ds/ds/pipeline/gtr/utils.py
@@ -1,1 +1,48 @@
+import os
+import sys
+import configparser
+
+import pymysql
+from sqlalchemy import create_engine
+
 gtr_ai_tags = ['artificial intelligence']
+
+query_ai_topics = (
+	"SELECT gtr_link_table.project_id "\
+	"FROM gtr_topic, gtr_link_table "\
+	"WHERE gtr_topic.id=gtr_link_table.id AND gtr_topic.text LIKE %s"
+	)
+
+query_ai_orgs = (
+	"SELECT gtr_organisations.name, gtr_organisations.id, count(gtr_link_table.project_id), "\
+	"gtr_organisations_locations.latitude, gtr_organisations_locations.longitude "\
+	"FROM gtr_link_table, gtr_organisations, gtr_organisations_locations "\
+	"WHERE gtr_organisations.id=gtr_organisations_locations.id "\
+	"AND gtr_organisations.id=gtr_link_table.id "\
+	"AND gtr_link_table.project_id IN %(l)s"\
+	"GROUP BY gtr_organisations.id"
+	)
+
+query_ai_orgs_all_topics = (
+	"SELECT gtr_link_table.id, count(gtr_link_table.project_id) "\
+	"FROM gtr_link_table "\
+	"WHERE gtr_link_table.id IN  %(l)s"\
+	"GROUP BY gtr_link_table.id"
+	)
+
+def est_conn(dbname='production'):
+
+	SQL_DB_CREDS = os.environ.get('SQL_DB_CREDS')
+
+	config = configparser.ConfigParser()
+	try:
+		config.read(SQL_DB_CREDS)
+	except TypeError:
+		print(f"Try setting SQL_DB_CREDS environmental variable to location of Nesta SQL credentials")
+
+	user = config['client']['user']
+	password = config['client']['password']
+	host = config['client']['host']
+
+	conn = create_engine(f'mysql+pymysql://{user}:{password}@{host}/{dbname}')
+	return conn
