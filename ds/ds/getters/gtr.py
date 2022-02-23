ffafb88d3b3b27d78c50809d51b88dfb2b08b57c
lizgzil <lizgzil@hotmail.com>
2022-02-23 16:51:39 +0000

    Data getter for gtr ai org filtered data

--- ds/ds/getters/gtr.py
+++ ds/ds/getters/gtr.py
@@ -5,6 +5,7 @@ from functools import lru_cache
 from metaflow import Flow, Run, Step
 from metaflow.exception import MetaflowNotFound
 from typing import Optional
+from pandas import DataFrame
 
 try:  # Hack for type-hints on attributes
     import pandas as pd
@@ -21,33 +22,33 @@ def get_run(flow_name: str) -> Run:
     except StopIteration as exc:
         raise MetaflowNotFound("Matching run not found") from exc
 
+def get_local_run(flow_name: str) -> Run:
+    """Gets last successful run executed with `--production`"""
+    runs = Flow(flow_name).runs()
+    try:
+        return next(filter(lambda run: run.successful, runs))
+    except StopIteration as exc:
+        raise MetaflowNotFound("Matching run not found") from exc
 
 
-Step('GtR_AI/1645464822243733/get_ai_orgs').task.data
+def get_gtr_ai_orgs(run: Optional[Run] = None, local: Optional[bool] = False) -> DataFrame:
+    """get the AI research organisations with lon/lat
+    Arguments:
+        run: what run to get (if None it gets the lastest run)
+        local: if False will get latest production run only
+    Returns:
+        Columns:
+            Name: Name, dtype: str, name of organisation
+            Name: Latitude, dtype: float, latitude of organisation
+            Name: Longitude, dtype: float, longitude of organisation
+    """
 
+    if run is None:
+        if local:
+            run = get_local_run("GtR_AI")
+        else:
+            run = get_run("GtR_AI")
 
-# def get_gtr_ai_orgs(run: Optional[Run] = None):
-#     """get the GVA in a local authority
-#     Arguments:
-#         run: what run to get (if None it gets the lastest production run)
-#     Returns:
-#         Columns:
-#             Name: nuts1_name, dtype: str, NUTS1 region (e.g. Scotland, South East etc)
-#             Name: la_code, dtype: str, local authority code
-#             Name: la_name, dtype: str, local authority name
-#             Name: year dtype: int, year (ranges between 1998 and 2019
-#             Name: gva dtype: float, £M Gross value added
-#     """
-
-#     if run is None:
-#         run = get_run("GtR_AI")
-
-#     return (
-#         pd.DataFrame(run.data.gva)
-#         .melt(
-#             id_vars=["itl1_region", "la_code", "la_name"],
-#             var_name="year",
-#             value_name="gva",
-#         )
-#         .rename(columns={"itl1_region": "nuts1_name"})
-#     )
+    return run.data.ai_org_ids_df_filtered.dropna(
+        subset=['Longitude','Latitude']
+        ).reset_index(drop=True)[['Name', 'Latitude', 'Longitude']]
