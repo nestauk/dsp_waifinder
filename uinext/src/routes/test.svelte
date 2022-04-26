66bcb2e4f0f604241b6614d4c095abf952aa26d4
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2022-04-26 19:58:01 -0300

    uinext: map route, add responsive layout Closes #54

---
+++ uinext/src/routes/test.svelte
@@ -0,0 +1,17 @@
+<script>
+	import ViewsXor from 'app/components/ViewPorts/ViewsXor.svelte'
+	import MaybeView from 'app/components/ViewPorts/MaybeView.svelte'
+	import Small from 'app/components/Small.svelte';
+	import Quads from 'app/components/Quads.svelte';
+</script>
+
+<ViewsXor
+	currentViewId='1'
+>
+	<MaybeView id='0'>
+		<Quads />
+	</MaybeView>
+	<MaybeView id='1'>
+		<Small/>
+	</MaybeView>
+</ViewsXor>
