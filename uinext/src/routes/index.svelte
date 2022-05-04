16cfe5f94e696584bb426ae3815ca00ad0cb0caa
Sebastian Ferreyra <ushiferreyra@gmail.com>
2022-05-04 15:31:20 -0300

    ui v2: added results panel (#57)
    Closes #56

--- uinext/src/routes/index.svelte
+++ uinext/src/routes/index.svelte
@@ -1,21 +1,33 @@
 <script>
 	import {_screen}
 		from '@svizzle/ui/src/sensors/screen/ScreenSensor.svelte';
+	import {isClientSide} from '@svizzle/ui/src/utils/env';
+	import * as _ from 'lamb';
 
 	import Medium from 'app/components/homeScreen/Medium.svelte';
 	import Small from 'app/components/homeScreen/Small.svelte';
 	import View from 'app/components/ViewPorts/View.svelte';
 	import ViewsXor from 'app/components/ViewPorts/ViewsXor.svelte';
+
+	let organizations = [];
+
+	const loadData = async () => {
+		const response = await fetch('/data/ai_map_orgs_places.json');
+		const fulldata = await response.json();
+
+		organizations = _.takeFrom(fulldata?.orgs || [], 400);
+	}
+
+	$: isClientSide && loadData();
+	$: viewId = $_screen?.sizes?.medium ? 'medium': 'small';
 </script>
 
-<ViewsXor
-	viewId='{$_screen?.sizes?.medium? 'medium':'small'}'
->
+<ViewsXor {viewId}>
 	<View id='medium'>
-		<Medium />
+		<Medium {organizations}/>
 	</View>
 	<View id='small'>
-		<Small/>
+		<Small {organizations}/>
 	</View>
 </ViewsXor>
 
