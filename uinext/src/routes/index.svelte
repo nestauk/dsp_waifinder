04838fbef969135ae7fb986064eec13fe5fbc91c
Luca Bonavita <mindrones@users.noreply.github.com>
2022-05-20 13:38:49 +0100

    ui v2: add a barchart of amount of orgs by place
    closes #75
    
    Also:
    - moved related code in `homeScreen/medium|small`
    - split `app/stores` into modules

--- uinext/src/routes/index.svelte
+++ uinext/src/routes/index.svelte
@@ -1,50 +1,40 @@
 <script>
-	import {_screen}
-		from '@svizzle/ui/src/sensors/screen/ScreenSensor.svelte';
 	import {isClientSide} from '@svizzle/ui/src/utils/env';
-	import {getId} from '@svizzle/utils';
 	import * as _ from 'lamb';
 
-	import Medium from 'app/components/homeScreen/Medium.svelte';
-	import Small from 'app/components/homeScreen/Small.svelte';
+	import Medium from 'app/components/homeScreen/medium/Medium.svelte';
+	import Small from 'app/components/homeScreen/small/Small.svelte';
 	import View from 'app/components/ViewPorts/View.svelte';
 	import ViewsXor from 'app/components/ViewPorts/ViewsXor.svelte';
+	import {_data, _orgs} from 'app/stores/data';
+	import {_screenId} from 'app/stores/layout';
+	import {setDefaultActiveView} from 'app/stores/navigation';
 	import {getLonLat} from 'app/utils/dataUtils';
-
-	let items;
 
 	const loadData = async () => {
 		const response = await fetch('/data/ai_map_annotated_orgs.json');
 		const json = await response.json();
 
 		if (json) {
-			const {orgs, places, org_types} = json;
-			const placesById = _.index(places, getId);
-			items = _.map(orgs,
-				org => ({
-					...org,
-					place: placesById[org.place_id],
-					types: _.map(org.types, typeId => org_types[typeId])
-				})
-			)
+			_data.set(json);
 		}
 	}
 
 	$: isClientSide && loadData();
-	$: viewId = $_screen?.sizes?.medium ? 'medium': 'small';
+	$: $_screenId && setDefaultActiveView();
 </script>
 
-<ViewsXor {viewId}>
+<ViewsXor viewId={$_screenId}>
 	<View id='medium'>
 		<Medium
 			{getLonLat}
-			{items}
+			items={$_orgs}
 		/>
 	</View>
 	<View id='small'>
 		<Small
 			{getLonLat}
-			{items}
+			items={$_orgs}
 		/>
 	</View>
 </ViewsXor>
