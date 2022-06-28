e7812fa887fc6912be14f9e655659d96e345931b
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2022-06-28 09:06:53 -0300

    Added text description and topic pill

--- uinext/src/routes/eval.svelte
+++ uinext/src/routes/eval.svelte
@@ -2,7 +2,12 @@
 	import {isClientSide} from '@svizzle/ui/src/utils/env';
 	import FingerprintJS from '@fingerprintjs/fingerprintjs';
 
+	import Pill from 'app/components/orgs/Pill.svelte';
+	import LayoutHMF from 'app/components/svizzle/LayoutHMF.svelte';
 	import {_deviceId} from 'app/stores/device';
+	import {getTopicLabel} from 'app/utils/dataUtils';
+
+	import {responseSample} from 'app/utils/temp.js';
 
 	const getDeviceId = async () => {
 		$_deviceId =
@@ -11,7 +16,49 @@
 			.then(result => result.visitorId);
 	}
 
+	let currentEntityId;
+
 	$: isClientSide && getDeviceId();
+
+	$: source = responseSample._source;
+	$: entities = source.dbpedia_entities;
+	$: entities, (currentEntityId = 0);
+	$: currentEntity = entities[currentEntityId];
+
+	$: description = source.description?.replace(
+		currentEntity.surfaceForm,
+		`<span>${currentEntity.surfaceForm}</span>`
+	);
+	$: score = currentEntity.confidence;
+	$: label = getTopicLabel(
+		currentEntity.URI.replace('http://dbpedia.org/resource/', '')
+	);
 </script>
 
-{$_deviceId}
+<div class='eval'>
+	<LayoutHMF>
+		<div slot='header'>
+			User: {$_deviceId}
+		</div>
+		<div class='main' slot='main'>
+			<p>
+				{@html description}
+			</p>
+		</div>
+		<div slot='footer' class='footer'>
+			<Pill
+				{label}
+				{score}
+			/>
+		</div>
+	</LayoutHMF>
+</div>
+
+<style>
+	.footer {
+		display: flex;
+	}
+	:global(.eval .main p span) {
+		background: yellow;
+	}
+</style>
