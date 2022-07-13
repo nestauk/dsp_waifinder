9902200ec5d7a7c8e3353b879f77dbbacb05d278
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2022-07-13 11:09:22 -0300

    Added relevant machines and commented potentially unused code

--- uinext/src/routes/index.svelte
+++ uinext/src/routes/index.svelte
@@ -1,4 +1,6 @@
 <script>
+	import {onMount} from 'svelte';
+	import {stores} from '@sapper/app';
 	import {isClientSide} from '@svizzle/ui/src/utils/env';
 
 	import Medium from 'app/components/homeScreen/medium/Medium.svelte';
@@ -6,11 +8,33 @@
 	import View from 'app/components/viewports/View.svelte';
 	import ViewsXor from 'app/components/viewports/ViewsXor.svelte';
 	import {toolName} from 'app/config';
+	import { createMapMachine } from 'app/machines/map/route';
+	import { parseParams } from 'app/machines/builder/formediting.options';
+
 	import {_orgs} from 'app/stores/data';
 	import {updateDataset} from 'app/stores/dataset';
 	import {_screenId} from 'app/stores/layout';
 	import {setDefaultActiveView} from 'app/stores/navigation';
 	import {getLonLat} from 'app/utils/dataUtils';
+
+
+	const { machine: routeMachine, contextStores: {
+		// config
+		runQueryOnSelect,
+		hideDisabledForms,
+		hideDisabledAggregations,
+		hideDisabledDatasets,
+		hideDisabledFields,
+		selectedForm,
+		selectedRequestTab,
+		showFullResponse,
+		resultSize,
+		forms,
+		dataset,
+		// docs
+		activeDocs,
+		aggDocText,
+	}} = createMapMachine();
 
 	const loadData = async () => {
 		const response = await fetch('/data/ai_map_annotated_orgs.json');
@@ -18,6 +42,41 @@
 
 		json && updateDataset(json);
 	}
+
+	const { page } = stores();
+	let eventType = 'READY';
+	onMount(async () => {
+		const loadPage = ({params}) => {
+			const event = {
+				query: params.q && rison.decode(params.q),
+			};
+			routeMachine.send(eventType);
+			parseParams(routeMachine, event);
+			eventType = 'ROUTE_CHANGED';
+		};
+
+		const pageReloader = () => {
+			const urlParams = new URL(document.location.toString()).searchParams;
+			loadPage({
+				params: _.fromPairs(Array.from(urlParams.entries()))
+			});
+		};
+		addEventListener('popstate', pageReloader);
+		const unsubscribe = page.subscribe(pageReloader);
+
+		if (process.env.INSPECT === 'true') {
+			const module = await import('@xstate/inspect');
+			module.inspect({
+				url: "https://statecharts.io/inspect",
+				iframe: false
+			});
+		}
+
+		return () => {
+			removeEventListener('popstate', pageReloader);
+			unsubscribe?.();
+		};
+	});
 
 	$: isClientSide && loadData();
 	$: $_screenId && setDefaultActiveView();
