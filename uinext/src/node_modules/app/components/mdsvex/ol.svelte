7bb6dc307004573a35098bf76fda3ec44d9acd6e
Luca Bonavita <mindrones@users.noreply.github.com>
2022-08-15 14:28:28 +0100

    ui v2: mdsvex, added more HTML elements adapters
    close #196

---
+++ uinext/src/node_modules/app/components/mdsvex/ol.svelte
@@ -0,0 +1,9 @@
+<ol {...$$props}>
+	<slot/>
+</ol>
+
+<style>
+	ol {
+		padding: 0.5rem 0 0.5rem 2rem;
+	}
+</style>
---
+++ uinext/src/node_modules/app/components/mdsvex/table.svelte
@@ -0,0 +1,10 @@
+<table {...$$props}><slot/></table>
+
+<style>
+	table {
+		border-collapse: collapse;
+		table-layout: fixed;
+		width: 100%;
+		word-break: break-word;
+	}
+</style>
