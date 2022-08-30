af5db6c57772de81b3ed475f7e1ff4b849b35d8a
Sebastian Ferreyra <ushiferreyra@gmail.com>
2022-08-30 13:05:27 -0300

    ui v2: ui v2: fix persistent scrolling across sections (#226)
    Closes #224
    
    Also:
    - Replaced import and deleted `uinext/src/node_modules/app/utils/env.js`

--- uinext/src/routes/methodology/_layout.svelte
+++ uinext/src/routes/methodology/_layout.svelte
@@ -1,15 +1,19 @@
 <script>
+	import {stores} from '@sapper/app';
 	import * as _ from 'lamb';
 
 	import ChevronLeft from '@svizzle/ui/src/icons/feather/ChevronLeft.svelte';
 	import ChevronRight from '@svizzle/ui/src/icons/feather/ChevronRight.svelte';
 	import Icon from '@svizzle/ui/src/icons/Icon.svelte';
 	import {_screen} from '@svizzle/ui/src/sensors/screen/ScreenSensor.svelte';
+	import {isClientSide} from '@svizzle/ui/src/utils/env';
 
 	import {isNotNil} from '@svizzle/utils';
 
 	import Link from 'app/components/svizzle/Link.svelte';
 	import {_currThemeVars} from 'app/stores/theme';
+
+	const {page: _page} = stores();
 
 	const segments = ['orgs', 'topics'];
 	const titles = {
@@ -19,11 +23,19 @@
 
 	export let segment;
 
+	let contentElement;
+
 	$: currentValueIndex = _.findIndex(segments, _.is(segment));
 	$: prevSegment = segments[currentValueIndex - 1];
 	$: nextSegment = segments[currentValueIndex + 1];
 	$: hasPrevSegment = isNotNil(prevSegment);
 	$: hasNextSegment = isNotNil(nextSegment);
+
+	$: {
+		// eslint-disable-next-line no-unused-expressions
+		$_page;
+		isClientSide && contentElement?.scrollTo(0, 0);
+	}
 </script>
 
 <main class='_layout methodology {$_screen?.classes}'>
@@ -86,7 +98,7 @@
 				</div>
 			{/if}
 		</menu>
-		<div>
+		<div bind:this={contentElement}>
 			<slot />
 		</div>
 	</section>
