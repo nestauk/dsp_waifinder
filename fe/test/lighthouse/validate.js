3b6fd6c677baf37c30578ba89149b09ec66542a3
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2022-09-02 22:21:23 -0300

    Made lighthouse work more robustly

--- fe/test/lighthouse/validate.js
+++ fe/test/lighthouse/validate.js
@@ -15,50 +15,60 @@ queue.on('end', () => {
 });
 
 const auditURL = async (id, url) => {
-	const chrome = await chromeLauncher.launch({
-		chromeFlags: [
-			'--headless',
-			// uncomment below if there are problems running the tests
-			// '--no-sandbox',
-		]
-	});
-	const options = {
-		logLevel: 'info',
-		output: 'html',
-		formFactor: 'desktop',
-		screenEmulation: {
-			disabled: true
-		},
-		// for performance results to be meaningful
-		// a reliable testing environment must be configured
-		onlyCategories: [
-			'accessibility',
-			'best-practices',
-			// 'performance',
-			'seo'
-		],
-		port: chrome.port
-	};
-	const runnerResult = await lighthouse(
-		urlBases.development + url,
-		options
-	);
-	const reportHtml = runnerResult.report.replaceAll(urlBases.development, '');
-
-	// eslint-disable-next-line no-sync
-	fs.writeFileSync(`static/audits/lighthouse/${id}.html`, reportHtml);
-
-	// `.lhr` is the Lighthouse Result as a JS object
-	console.log(
-		'Report is done for',
-		runnerResult.lhr.finalUrl
-	);
-	console.log(
-		'Accessibility score was',
-		runnerResult.lhr.categories.accessibility.score * 100
-	);
-
-	await chrome.kill();
+	console.log('Launching chrome...')
+	try {
+		const chrome = await chromeLauncher.launch({
+			port: 9222,
+			chromeFlags: [
+				'--headless',
+				// uncomment below if there are problems running the tests
+				// '--no-sandbox',
+			]
+		});
+		console.log('Launched chrome succesfully.')
+		const options = {
+			logLevel: 'info',
+			output: 'html',
+			formFactor: 'desktop',
+			screenEmulation: {
+				disabled: true
+			},
+			// for performance results to be meaningful
+			// a reliable testing environment must be configured
+			onlyCategories: [
+				'accessibility',
+				'best-practices',
+				// 'performance',
+				'seo'
+			],
+			port: chrome.port
+		};
+		const runnerResult = await lighthouse(
+			urlBases.development + url,
+			options
+		);
+		const reportHtml = runnerResult.report.replaceAll(urlBases.development, '');
+	
+		// eslint-disable-next-line no-sync
+		fs.writeFileSync(`static/audits/lighthouse/${id}.html`, reportHtml);
+	
+		// `.lhr` is the Lighthouse Result as a JS object
+		console.log(
+			'Report is done for',
+			runnerResult.lhr.finalUrl
+		);
+		console.log(
+			'Accessibility score was',
+			runnerResult.lhr.categories.accessibility.score * 100
+		);
+	}
+	catch (e) {
+		console.error('Error!', e);
+	}
+	finally {
+		await chrome.kill();
+		console.log('Killed chrome.');
+	}
 }
 
 const enqueueTask = ([id, url]) =>
