9350c7ef6056ce8dad9d5db7cd5ba668cfa97fb4
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2022-09-13 11:11:12 -0300

    upgraded deps

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -1,11 +1,10 @@
-import {sveltekit} from '@sveltejs/kit/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 const config = {
 	plugins: [sveltekit()],
 	server: {
 		fs: {
 			// Allow serving files from one level up to the project root
-			// so `fe/src/lib/utils/version.js` may import from `package.json`
 			allow: ['..']
 		}
 	}
