a861cfa878ec38967e6805f73d657d7f8fb19ce4
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2022-09-14 11:57:29 -0300

    final env vars

--- fe/src/lib/env.js
+++ fe/src/lib/env.js
@@ -1,9 +1,5 @@
-import {PUBLIC_BACKEND_BASE, PUBLIC_TEST} from '$env/static/public';
+import {PUBLIC_BACKEND_BASE} from '$env/static/public';
 
 export const isDev = import.meta.env.DEV;
 
-export const backendBase = import.meta.env.VITE_BACKEND_BASE;
-
-console.log('dev', isDev);
-console.log('backendBase', backendBase, PUBLIC_BACKEND_BASE);
-console.log('PUBLIC_TEST', PUBLIC_TEST);
+export const backendBase = PUBLIC_BACKEND_BASE;
