00b9eae1999dc203a4b8e33a9a64c3577379a23f
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2022-09-14 11:30:24 -0300

    one more test

--- fe/src/lib/env.js
+++ fe/src/lib/env.js
@@ -1,4 +1,4 @@
-import {PUBLIC_BACKEND_BASE} from '$env/static/public';
+import {PUBLIC_BACKEND_BASE, PUBLIC_TEST} from '$env/static/public';
 
 export const isDev = import.meta.env.DEV;
 
@@ -6,3 +6,4 @@ export const backendBase = import.meta.env.VITE_BACKEND_BASE;
 
 console.log('dev', isDev);
 console.log('backendBase', backendBase, PUBLIC_BACKEND_BASE);
+console.log('PUBLIC_TEST', PUBLIC_TEST);
